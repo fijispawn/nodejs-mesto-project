112fecb51d3d43f9bdb16c4b7044312f42d2f362
Mikhail Smirnov <m2004029@edu.misis.ru>
2025-10-20 16:06:53 +0300

    исправление конфига

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -20,10 +20,12 @@ module.exports = {
         "git@github.com:fijispawn/nodejs-mesto-project.git",
       path: process.env.DEPLOY_PATH || "/home/user/apps/mesto-api",
 
+      ssh_options: [`-i ${process.env.DEPLOY_SSH_KEY || "~/.ssh/mesto-vm"}`],
+
       "pre-deploy-local":
         "set -a; [ -f .env.deploy ] && . ./.env.deploy; set +a; " +
-        "ssh -i ${DEPLOY_SSH_KEY:~/.ssh/mesto-vm} $DEPLOY_USER@$DEPLOY_HOST 'mkdir -p $DEPLOY_PATH/shared' && " +
-        "scp -i ${DEPLOY_SSH_KEY:~/.ssh/mesto-vm} .env $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH/shared/.env",
+        "ssh -i ~/.ssh/mesto-vm user@51.250.34.72 'mkdir -p /home/user/apps/mesto-api/shared' && " +
+        "scp -i ~/.ssh/mesto-vm .env user@51.250.34.72:/home/user/apps/mesto-api/shared/.env",
 
       "post-deploy":
         "cd current && npm ci && cp ../shared/.env .env && pm2 reload ecosystem.config.js --env production",
